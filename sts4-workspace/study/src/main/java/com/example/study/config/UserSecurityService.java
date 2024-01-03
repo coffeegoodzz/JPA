cb3fdcc83424e2bfe34e962077f3bd50424a316e
coffeegoodzz <ejsvk9811@naver.com>
2024-01-04 06:18:05 +0900

    01.04 security 활용하여 회원가입 로그인 로그아웃 연습 & 게시물 답변 개수 표시추가

---
+++ sts4-workspace/study/src/main/java/com/example/study/config/UserSecurityService.java
@@ -0,0 +1,48 @@
+package com.example.study.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.study.Entity.SiteUser;
+import com.example.study.Repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+	
+	private final UserRepository userRepository;
+	
+	
+	// loadUserByUsername -> 사용자명으로 비밀번호를 조회하여 리턴하는 메소드
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+		
+		if(siteUser.isEmpty()) {
+			throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+		}
+		SiteUser siteUser2 = siteUser.get();
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		if("admin".equals(username)) {
+			authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+		}else {
+			authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+		}
+		// security는 내부적으로 입력받은 비밀번호와 일치하는지 검사하는 로직을 가지고 있음
+		return new User(siteUser2.getUsername(),siteUser2.getPassword(), authorities); 
+	}
+
+}
